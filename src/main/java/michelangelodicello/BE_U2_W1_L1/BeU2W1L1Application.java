0d4752ee55f0621d65555a4765e795b29f1d10b9
dicellomichelangelo-debug <dicellomichelangelo@gmail.com>
2026-06-30 15:59:35 +0200

    Initial commit

---
+++ src/main/java/michelangelodicello/BE_U2_W1_L1/BeU2W1L1Application.java
@@ -0,0 +1,13 @@
+package michelangelodicello.BE_U2_W1_L1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2W1L1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2W1L1Application.class, args);
+	}
+
+}
---
+++ src/test/java/michelangelodicello/BE_U2_W1_L1/BeU2W1L1ApplicationTests.java
@@ -0,0 +1,13 @@
+package michelangelodicello.BE_U2_W1_L1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2W1L1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
